e3861c500e6e6b6b981c41c728d1bd42d27242df
binogood <binogood68@gmail.com>
2022-04-06 23:45:46 +0900

    ENV 설정에 따라 development환경에서 실행될 CarFakeRepo 클래스를 작성합니다.

--- app/car/repository/car.py
+++ app/car/repository/car.py
@@ -1,5 +1,7 @@
+import datetime
+
 from abc import ABCMeta, abstractmethod
-from typing import Optional
+from typing import Optional, List
 
 from app.car.models.car import Car
 from core.db.session import session
@@ -33,3 +35,29 @@ class CarMySQLRepo(CarRepo):
         return car
 
 
+class CarFakeRepo(CarRepo):
+    cars: List = []
+    db_car_id = 1
+    created_at: datetime = datetime.datetime.now()
+    updated_at: datetime = datetime.datetime.now()
+
+    def __init__(self):
+        print(CarFakeRepo.cars)
+
+    async def get_by_car_trim(self, trim_id: int) -> Optional[Car]:
+        for item_car_trim in CarFakeRepo.cars:
+            if item_car_trim.trim_id == trim_id:
+                return item_car_trim
+
+    async def get_by_car_id(self, car_id: int) -> Optional[Car]:
+        for item_car_id in CarFakeRepo.cars:
+            if item_car_id.car_id == car_id:
+                return item_car_id
+
+    async def save(self, car: Car) -> Car:
+        car.car_id = CarFakeRepo.db_car_id
+        car.created_at = CarFakeRepo.created_at
+        car.updated_at = CarFakeRepo.updated_at
+        CarFakeRepo.db_car_id += 1
+        CarFakeRepo.cars.append(car)
+        return car

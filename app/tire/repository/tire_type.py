48eee316f798fd84ecf476823806b2f6125a38ff
binogood <binogood68@gmail.com>
2022-04-06 23:46:53 +0900

    ENV 설정에 따라 development 환경에서 실행될 TireTypeFakeRepo 클래스를 작성합니다.

--- app/tire/repository/tire_type.py
+++ app/tire/repository/tire_type.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-from typing import Optional
+from typing import Optional, List
 
 from app.tire.models.tire_type import TireType
 from core.db.session import session
@@ -23,4 +23,21 @@ class TireTypeMySQLRepo(TireTypeRepo):
 
     async def save(self, tire_type: TireType) -> TireType:
         session.add(tire_type)
+        return tire_type
+
+
+class TireTypeFakeRepo(TireTypeRepo):
+    tire_types: List = []
+    db_tire_types_id = 1
+
+    async def get_by_tire_type(self, name: str) -> Optional[TireType]:
+        for item_tire_type in TireTypeFakeRepo.tire_types:
+            if item_tire_type.name == name:
+                return item_tire_type
+
+    async def save(self, tire_type: TireType) -> TireType:
+        tire_type.tire_type_id = TireTypeFakeRepo.db_tire_types_id
+        TireTypeFakeRepo.db_tire_types_id += 1
+        TireTypeFakeRepo.tire_types.append(tire_type)
+        print(TireTypeFakeRepo.tire_types)
         return tire_type

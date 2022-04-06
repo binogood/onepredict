7924778ff7efac0f672ab1dedbc7d8c3415d08b7
binogood <binogood68@gmail.com>
2022-04-06 23:47:18 +0900

    ENV 설정에 따라 development 환경에서 실행될 UserCarFakeRepo 클래스를 작성합니다.

--- app/user/repository/user_car.py
+++ app/user/repository/user_car.py
@@ -2,6 +2,8 @@ from abc import ABCMeta, abstractmethod
 from typing import Optional
 
 from app.user.models.user_car import UserCar
+from app.user.repository.user import UserFakeRepo
+from app.car.repository.car import CarFakeRepo
 
 from core.db.session import session
 
@@ -27,4 +29,24 @@ class UserCarMySQLRepo(UserCarRepo):
 
     async def save(self, user_car: UserCar) -> UserCar:
         session.add(user_car)
+        return user_car
+
+
+class UserCarFakeRepo(UserCarRepo):
+    user_cars = []
+    user_car_id = 1
+
+    def __init__(self):
+        pass
+
+    async def get_by_user_car(self, car_id: int, user_id: int) -> Optional[UserCar]:
+        for item_user_car in UserCarFakeRepo.user_cars:
+            if item_user_car.car_id == car_id and item_user_car.user_id == user_id:
+                return item_user_car
+
+    async def save(self, user_car: UserCar) -> UserCar:
+        user_car.user_car_id = UserCarFakeRepo.user_car_id
+        UserCarFakeRepo.user_car_id += 1
+        UserCarFakeRepo.user_cars.append(user_car)
+        print(UserCarFakeRepo.user_cars)
         return user_car

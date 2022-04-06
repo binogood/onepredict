8ae2b9f6759bec1ce94b930dbb95e2ddfe01f2c5
binogood <binogood68@gmail.com>
2022-04-06 23:47:06 +0900

    ENV 설정에 따라 development 환경에서 실행될 UserFakeRepo 클래스를 작성합니다.

--- app/user/repository/user.py
+++ app/user/repository/user.py
@@ -1,5 +1,7 @@
+import datetime
+
 from abc import ABCMeta, abstractmethod
-from typing import Optional
+from typing import Optional, List
 
 from app.user.models.user import User
 from core.db.session import session
@@ -30,4 +32,34 @@ class UserMySQLRepo(UserRepo):
 
     async def save(self, user: User) -> User:
         session.add(user)
+        return user
+
+
+class UserFakeRepo(UserRepo):
+    users: List = []
+    db_user_id: int = 1
+    created_at: datetime = datetime.datetime.now()
+    updated_at: datetime = datetime.datetime.now()
+
+    def __init__(self):
+        pass
+
+    async def get_by_id(self, user_id: int) -> Optional[User]:
+        for item_id in UserFakeRepo.users:
+            if item_id.user_id == user_id:
+                return item_id
+
+    async def get_by_name(self, name: str) -> Optional[User]:
+        for item_name in UserFakeRepo.users:
+            if item_name.name == name:
+                return item_name
+
+    async def save(self, user: User) -> User:
+        user.is_admin = True
+        user.user_id = UserFakeRepo.db_user_id
+        user.created_at = UserFakeRepo.created_at
+        user.updated_at = UserFakeRepo.updated_at
+        UserFakeRepo.db_user_id += 1
+        UserFakeRepo.users.append(user)
+        print(UserFakeRepo.users)
         return user

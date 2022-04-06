82b7805e4fdb0ac23bec8fe2908f0e99ca820e4d
binogood <binogood68@gmail.com>
2022-04-06 23:44:59 +0900

    ENV 설정에 따라 실행될 fake_init_di() 함수를 작성합니다.

--- core/di/__init__.py
+++ core/di/__init__.py
@@ -1,10 +1,10 @@
 from pythondi import Provider, configure
-from app.user.repository.user import UserRepo, UserMySQLRepo
-from app.user.repository.user_car import UserCarRepo, UserCarMySQLRepo
-from app.tire.repository.tire import TireRepo, TireMySQLRepo
-from app.tire.repository.tire_type import TireTypeRepo, TireTypeMySQLRepo
-from app.car.repository.car import CarRepo, CarMySQLRepo
-from app.car.repository.car_tire import CarTireRepo, CarTireMySQLRepo
+from app.user.repository.user import UserRepo, UserMySQLRepo, UserFakeRepo
+from app.user.repository.user_car import UserCarRepo, UserCarMySQLRepo, UserCarFakeRepo
+from app.tire.repository.tire import TireRepo, TireMySQLRepo, TireFakeRepo
+from app.tire.repository.tire_type import TireTypeRepo, TireTypeMySQLRepo, TireTypeFakeRepo
+from app.car.repository.car import CarRepo, CarMySQLRepo, CarFakeRepo
+from app.car.repository.car_tire import CarTireRepo, CarTireMySQLRepo, CarTireFakeRepo
 
 
 def init_di():
@@ -15,4 +15,15 @@ def init_di():
     provider.bind(TireTypeRepo, TireTypeMySQLRepo)
     provider.bind(CarRepo, CarMySQLRepo)
     provider.bind(CarTireRepo, CarTireMySQLRepo)
+    configure(provider=provider)
+
+
+def fake_init_di():
+    provider = Provider()
+    provider.bind(UserRepo, UserFakeRepo)
+    provider.bind(UserCarRepo, UserCarFakeRepo)
+    provider.bind(TireRepo, TireFakeRepo)
+    provider.bind(TireTypeRepo, TireTypeFakeRepo)
+    provider.bind(CarRepo, CarFakeRepo)
+    provider.bind(CarTireRepo, CarTireFakeRepo)
     configure(provider=provider)
